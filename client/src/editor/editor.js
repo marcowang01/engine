28de0906ebbd67c19cf0febb8d743cc65f148880
marcowang01 <marcowang01@gmail.com>
2024-11-03 00:08:32 -0700

    code editor done

--- client/src/editor/editor.js
+++ client/src/editor/editor.js
@@ -1,8 +1,46 @@
-import { python } from "@codemirror/lang-python"
-import { EditorView, minimalSetup } from "codemirror"
-let editor = new EditorView({
-  extensions: [minimalSetup, python()],
-  parent: document.getElementById("editor-parent"),
-})
+import { EditorView } from "codemirror"
 
-export default editor
+import { defaultHighlightStyle, indentOnInput, syntaxHighlighting } from "@codemirror/language"
+import { EditorState } from "@codemirror/state"
+import {
+  drawSelection,
+  highlightActiveLine,
+  highlightSpecialChars,
+  lineNumbers,
+} from "@codemirror/view"
+
+// Theme
+import { oneDark } from "@codemirror/theme-one-dark"
+
+// Language
+import { python } from "@codemirror/lang-python"
+
+function createEditorState(initialContent, options = {}) {
+  let extensions = [
+    lineNumbers(),
+    highlightSpecialChars(),
+    drawSelection(),
+    indentOnInput(),
+    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+    highlightActiveLine(),
+    python(),
+  ]
+
+  if (options.oneDark) {
+    extensions.push(oneDark)
+  }
+
+  return EditorState.create({
+    doc: initialContent,
+    extensions,
+  })
+}
+
+function createEditorView(state, parent) {
+  return new EditorView({ state, parent })
+}
+
+const initialState = createEditorState("print('Hello, world!')\n", { oneDark: true })
+const editorView = createEditorView(initialState, document.getElementById("editor-parent"))
+
+export default editorView
