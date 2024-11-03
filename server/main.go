d48af5286cc2f8e39e2cf3b89dd416e0779025b2
marcowang01 <marcowang01@gmail.com>
2024-11-03 02:07:22 -0800

    basic server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+const PORT = ":8080"
+
+type ExecuteCodeRequest struct {
+	Code     string `json:"code" validate:"required"`
+	Language string `json:"language" validate:"required"`
+}
+
+
+func executeCode(w http.ResponseWriter, r *http.Request) {
+	var request ExecuteCodeRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Struct(request); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "executing code %s in language %s", request.Code, request.Language)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/execute-code", executeCode)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Printf("staring server on port %s\n", PORT)
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
